09ac1c8fadad75800c412a5ee1849253fcaa7c9b
Kai <kai@kana.jetzt>
2024-01-06 12:20:20 +0100

    refactor: :recycle: moved vars inside function scope

--- mods-unpacked/Pasha-TwitchEBS/mod_main.gd
+++ mods-unpacked/Pasha-TwitchEBS/mod_main.gd
@@ -5,17 +5,15 @@ const PASHA_TWITCHEBS_DIR = "Pasha-TwitchEBS"
 var OAuthTokenFetcher = load("res://mods-unpacked/Pasha-TwitchEBS/oauth_token_fetcher.gd")
 var PubsubSender = load("res://mods-unpacked/Pasha-TwitchEBS/pubsub_sender.gd")
 
-var mod_dir_path := ""
-var extensions_dir_path := ""
-var translations_dir_path := ""
 
 func _init(modloader = ModLoader) -> void:
-	mod_dir_path = ModLoaderMod.get_unpacked_dir().plus_file(PASHA_TWITCHEBS_DIR)
 	install_script_extensions()
 
 
 func install_script_extensions() -> void:
-	extensions_dir_path = mod_dir_path.plus_file("extensions")
+	var mod_dir_path := ModLoaderMod.get_unpacked_dir().plus_file(PASHA_TWITCHEBS_DIR)
+	var extensions_dir_path := mod_dir_path.plus_file("extensions")
+
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("ui/menus/pages/main_menu.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("singletons/run_data.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("ui/menus/shop/stats_container.gd"))
